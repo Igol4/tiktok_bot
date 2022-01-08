10dd53cd20a2028ae1699e2ce2a6606b045eb491
Igol4 <94765726+Igol4@users.noreply.github.com>
2022-01-08 22:57:22 +0100

    Update tiktok_comment_bot.py

--- tiktok_comment_bot.py
+++ tiktok_comment_bot.py
@@ -1,6 +1,10 @@
+#IMPORTANT change numbers inside mouse.position depending on your resolution to match tiktok buttons, current positions are for 1920x1080
+#prints are for debugging
+#to avoid bans you may wanna change time.sleep values to random numbers in wich case you write time.sleep(random.randint(lower number, larger number)/10or100or1000...)
+#or add interval to pyautogui.typewrite(comment): pyautogui.typewrite(comment, interval=number)
+#although I did not receive a ban during script testing
 import webbrowser
 import os
-import subprocess
 import pynput
 from pynput.mouse import Button, Controller
 import time
@@ -30,42 +34,35 @@ with Listener(on_press=on_press, on_release=on_release) as listener:
     webbrowser.open('http://tiktok.com')
     mouse = Controller()
     #print ("Current position: " + str(mouse.position))
-    mouse.position = (997, 564)
+    mouse.position = (997, 564) 
     time.sleep(5)
     mouse.press(Button.left)
-    mouse.release(Button.left)
+    mouse.release(Button.left) #click on the video to make it bigger
     time.sleep(2)
     while(y < 15):
         mouse.position = (1452, 1040)
         mouse.press(Button.left)
-        mouse.release(Button.left)
+        mouse.release(Button.left) #click on the comment field
         y+=1
         r=str(f)
-        x=random.randrange(0, 5)
+        x=random.randrange(0, 5) #change the second number depending how many lines you have in comments.txt file (start counting from 0, also each line is one comment)
         #print(x)
-        print(f[x])
-        comment=str(f[x])
+        #print(f[x])
+        comment=str(f[x]) #pick comment from the list
         pyautogui.typewrite(comment)
         time.sleep(1.5)
                 
         mouse.position = (1334, 592)
         mouse.press(Button.left)
-        mouse.release(Button.left)
+        mouse.release(Button.left) #go to the next video
         time.sleep(1)
         mouse.position = (1452, 1040)
-        #mouse.press(Button.left)
-        #mouse.release(Button.left)
-        #y+=1
-        #z=random.randrange(0, 5)
-        #comment=str(f[z])
-        #pyautogui.typewrite(comment)
-        #mouse.position = (1452, 1040)
-        #time.sleep(1.5)
 
         follow=random.randint(1, 9999999999)
         like=random.randint(1, 9999999999)
         clink=random.randint(1, 9999999999)
-        if follow%11 == 0:
+        #it has a chance to follow user account, like video or copy video link
+        if follow%11 == 0: 
             mouse.position = (1859, 176)
             mouse.press(Button.left)
             mouse.release(Button.left)
@@ -77,6 +74,4 @@ with Listener(on_press=on_press, on_release=on_release) as listener:
             mouse.position = (1847, 364)
             mouse.press(Button.left)
             mouse.release(Button.left)
-        time.sleep(5)
     listener.join()
-    
